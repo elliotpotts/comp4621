6e67e9efcf6c128f10ff80902451649af684a975
Elliot Potts <elliotpotts@gmail.com>
2018-04-21 19:06:55 +0000

    Fixed error handling. Kinda implemented interrupting server but not sure it works lol

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,30 @@
 #include <iterator>
 #include <array>
 #include <memory>
+#include <atomic>
+#include <signal.h>
 
 const short http_port = 9999;//80;
 const int backlog = 128; // arbitrary
 const std::string crlf = "\r\n";
 const int buffer_size = 2048; // arbitraty
 
-void errmaybe() {
-    if(errno > 0) {
-        throw std::system_error(errno, std::system_category());
+void errmaybe(int return_val) {
+    if(return_val < 0 && errno > 0) {
+        throw std::system_error(errno, std::generic_category());
     }
 }
 
 using byte_buf = std::array<char, buffer_size>;
 
+// Line-buffered tcp socket
 class tcp_socket {
     int fd;
     byte_buf buffer;
     byte_buf::iterator data_begin;
     byte_buf::iterator data_end;
     public:
-    tcp_socket(int connected) : fd(connected), data_begin(begin(buffer)), data_end(begin(buffer) + 1) {
+    tcp_socket(int connected) : fd(connected), data_begin(begin(buffer)), data_end(begin(buffer)) {
     };
     std::string recvline() {
         std::ostringstream line;
@@ -40,10 +43,10 @@ class tcp_socket {
         while(line_end == data_end) {
             // Add what we have so far to our line
             std::copy(data_begin, data_end, std::ostream_iterator<char>(line));
+            // Read more data
             data_begin = begin(buffer);
-            // Read more data 
             int n = ::recv(fd, data_begin, buffer_size, 0);
-            errmaybe();
+            errmaybe(n);
             data_end = data_begin + n + 1;
             // Search for newline again
             line_end = std::search(data_begin, data_end, begin(crlf), end(crlf));
@@ -55,49 +58,63 @@ class tcp_socket {
     }
     ~tcp_socket() {
         ::close(fd);
-        errmaybe();
     }
 };
 
 class http_server {
     int fd;
     void handle_client(tcp_socket&& client) {
-        std::cout << "Got new client\n";
-        std::cout << client.recvline() << std::endl;
-        std::cout << client.recvline() << std::endl;
         std::cout << client.recvline() << std::endl;
     }
     public:
     http_server() : fd(::socket(AF_INET, SOCK_STREAM, 0)) {
-        errmaybe();
+        errmaybe(fd);
+        int enable_reuse = 1;
+        errmaybe(setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &enable_reuse, sizeof(enable_reuse)));
         const sockaddr_in listen_address = {
             AF_INET,           // sin_family
             htons(http_port),  // sin_port
             INADDR_ANY         // sin_addr
         };
-        ::bind(fd, reinterpret_cast<const sockaddr*>(&listen_address), sizeof(listen_address));
-        errmaybe();
-        ::listen(fd, backlog);
-        errmaybe();
+        errmaybe(::bind(fd, reinterpret_cast<const sockaddr*>(&listen_address), sizeof(listen_address)));
+        errmaybe(::listen(fd, backlog));
     }
     ~http_server() {
-        close(fd);
+        ::close(fd);
     }
-    
     void serve() {
         while(true) {
             sockaddr_in client_addr;
             socklen_t addrlen = sizeof(client_addr);
             int clientfd = ::accept(fd, reinterpret_cast<sockaddr*>(&client_addr), &addrlen);
-            errmaybe();
+            errmaybe(clientfd);
             handle_client({clientfd});
         }
     }
-    void shutdown();
 };
 
+void handle_signal(int) {
+}
+
 int main() {
-    std::cout << "Hello, World!\n";
-    http_server s;
-    s.serve();
+    // Install signal handler for SIGINT aka keyboard interrupt
+    struct sigaction sa;
+    sa.sa_handler = handle_signal;
+    //sa.sa_mask = 0;
+    sa.sa_flags = 0;
+    sa.sa_restorer = nullptr;
+    errmaybe(sigaction(SIGINT, &sa, nullptr));
+    
+    // Start server
+    std::cout << "Listening...\n";
+    try {
+        http_server s;
+        s.serve();
+    } catch (const std::system_error& ex) {
+        if (ex.code().value() == EINTR) {
+            std::cout << "Keyboard interrupt. Stopping server" << std::endl;
+        } else {
+            throw;
+        }
+    }
 }
